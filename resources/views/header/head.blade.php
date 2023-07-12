f14fd919fa29b4f37ae63fa285d80fc2bac4f2dd
AJ2235 <ariba2209f@aptechgdn.net>
2023-07-13 00:08:53 +0500

    header removed

---
+++ resources/views/header/head.blade.php
@@ -0,0 +1,103 @@
+<!-- main header -->
+<header class="main-header header-style-three">
+    <!-- header-lower -->
+    <div class="header-lower">
+        <div class="large-container">
+            <div class="outer-box">
+                <div class="logo-box">
+                    <figure class="logo"><a href="index.html"><img src="assets/images/logo.png" alt=""></a></figure>
+                </div>
+                <div class="menu-area">
+                    <!--Mobile Navigation Toggler-->
+                    <div class="mobile-nav-toggler">
+                        <i class="icon-bar"></i>
+                        <i class="icon-bar"></i>
+                        <i class="icon-bar"></i>
+                    </div>
+                    <nav class="main-menu navbar-expand-md navbar-light">
+                        <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                            <ul class="navigation clearfix">
+                                <li class="current dropdown"><a href="#">Home</a>
+                                    <ul>
+                                        <li><a href="index.html">Home Page 01</a></li>
+                                        <li><a href="index-2.html">Home Page 02</a></li>
+                                        <li><a href="index-3.html">Home Page 03</a></li>
+                                        <li class="dropdown"><a href="index.html">Header Style</a>
+                                            <ul>
+                                                <li><a href="index.html">Header Style 01</a></li>
+                                                <li><a href="index-2.html">Header Style 02</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="about.html">About</a></li>
+                                <li class="dropdown"><a href="#">Services</a>
+                                    <ul>
+                                        <li><a href="service.html">Our Services</a></li>
+                                        <li><a href="service-details.html">Agency Minimal</a></li>
+                                        <li><a href="service-details-2.html">Interior Design</a></li>
+                                        <li><a href="service-details-3.html">Urban Design</a></li>
+                                        <li><a href="service-details-4.html">Decor Plan</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">Projects</a>
+                                    <ul>
+                                        <li><a href="project.html">Our Project</a></li>
+                                        <li><a href="project-details.html">Project Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">Pages</a>
+                                    <ul>
+                                        <li><a href="team.html">Our Team</a></li>
+                                        <li><a href="team-details.html">Team Details</a></li>
+                                        <li><a href="testimonial.html">Testimonial</a></li>
+                                        <li><a href="coming-soon.html">Coming Soon</a></li>
+                                        <li><a href="error.html">404</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">Blog</a>
+                                    <ul>
+                                        <li><a href="blog.html">Our Blog</a></li>
+                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <div class="menu-right-content">
+                        <div class="search-box-outer search-toggler">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </div>
+                        <div class="nav-btn nav-toggler navSidebar-button">
+                            <img src="./assets/images/steel (1).png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--sticky Header-->
+    <div class="sticky-header">
+        <div class="outer-box">
+            <div class="logo-box">
+                <figure class="logo"><a href="index.html"><img src="assets/images/logo.png" alt=""></a></figure>
+            </div>
+            <div class="menu-area clearfix">
+                <nav class="main-menu clearfix">
+                    <!--Keep This Empty / Menu will come through Javascript-->
+                </nav>
+                <div class="menu-right-content">
+                    <div class="search-box-outer search-toggler">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                    <div class="nav-btn nav-toggler navSidebar-button">
+                        <img src="./assets/images/steel (1).png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- main-header end -->
