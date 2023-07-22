00f68d0a3ae6ebc4855da0c8eab63dcb48381bdb
AJ2235 <ariba2209f@aptechgdn.net>
2023-07-23 01:37:33 +0500

    controller

---
+++ app/Http/Controllers/usercontroller.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class usercontroller extends Controller
+{
+    public function index(){
+
+        return view("index");
+
+    }
+ public function index2(){
+
+        return view("index-2");
+        
+    }
+
+    public function contact(){
+
+        return view("contact");
+        
+    }
+
+    public function about(){
+
+        return view("about");
+        
+    }
+
+    public function blog(){
+
+        return view("blog");
+        
+    }
+    public function project(){
+
+        return view("project");
+        
+    }
+    public function service(){
+
+        return view("service");
+        
+    }
+    public function service_det(){
+
+        return view("service-details");
+        
+    }
+     public function service_det_2(){
+
+        return view("service-details-2");
+        
+    }
+
+    public function service_det_3(){
+
+        return view("service-details-3");
+        
+    }
+     public function service_det_4(){
+
+        return view("service-details-4");
+        
+    }
+    public function testimonial(){
+
+        return view("testimonial");
+        
+    }
+    
+    public function coming(){
+
+        return view("coming-soon");
+        
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\usercontroller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,42 +13,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/index', function () {
-    return view('index-2');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/blog', function () {
-    return view('blog');
-});
-Route::get('/coming-soon', function () {
-    return view('coming-soon');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
-Route::get('/project', function () {
-    return view('project');
-});
-Route::get('/service-details-2', function () {
-    return view('service-details-2');
-});
-Route::get('/service-details', function () {
-    return view('service-details');
-});
-Route::get('/service-details-3', function () {
-    return view('service-details-3');
-});
-Route::get('/service-details-4', function () {
-    return view('service-details-4');
-});
-Route::get('/service', function () {
-    return view('service');
-});
-Route::get('/testimonial', function () {
-    return view('testimonial');
+// Route::get('/', function () {
+//     return view('index');
+// });
+// Route::get('/index', function () {
+//     return view('index-2');
+// });
+// Route::get('/about', function () {
+//     return view('about');
+// });
+// Route::get('/blog', function () {
+//     return view('blog');
+// });
+// Route::get('/coming-soon', function () {
+//     return view('coming-soon');
+// });
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+// Route::get('/project', function () {
+//     return view('project');
+// });
+// Route::get('/service-details-2', function () {
+//     return view('service-details-2');
+// });
+// Route::get('/service-details', function () {
+//     return view('service-details');
+// });
+// Route::get('/service-details-3', function () {
+//     return view('service-details-3');
+// });
+// Route::get('/service-details-4', function () {
+//     return view('service-details-4');
+// });
+// Route::get('/service', function () {
+//     return view('service');
+// });
+// Route::get('/testimonial', function () {
+//     return view('testimonial');
+// });
+
+
+Route::controller(usercontroller::class)->group(function(){
+
+    Route::get('/','index');
+    Route::get('/index','index2');
+    Route::get('/about','about');
+    Route::get('/blog','blog');
+    Route::get('/contact','contact');
+    Route::get('/project','project');
+    Route::get('/service','service');
+    Route::get('/service-details','service_det');
+    Route::get('/service-details-2','service_det_2');
+    Route::get('/service-details-3','service_det_3');
+    Route::get('/service-details-4','service_det_4');
+    Route::get('/coming-soon','coming');
+    Route::get('/testimonial','testimonial');
+
+
+
+
 });
