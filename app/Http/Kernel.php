4426620e56625766f6c0f8ef6242dcefe67dc6f4
AJ2235 <ariba2209f@aptechgdn.net>
2023-08-04 02:09:04 +0500

    change

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+         'user'  =>  \App\Http\Middleware\usermiddleware::class,
+         'admin'  => \App\Http\Middleware\admin::class,
+
     ];
 }
